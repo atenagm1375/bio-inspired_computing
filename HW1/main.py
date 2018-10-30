e8a9d58357eb28f2110735bbe4e437bcaa99d7e0
atenagm <ashenatena@gmail.com>
2018-10-30 11:45:42 +0330

    fix main procedure

--- HW1/main.py
+++ HW1/main.py
@@ -8,7 +8,7 @@ stagnancy_num = 20
 p_c = 0.7
 p_m = 0.05
 p_rep = 0.25
-population_num = 10
+population_num = 100
 iteration_num = 1
 
 # genetic algorithm process
@@ -16,11 +16,19 @@ cities, distances, num_routes = read_data_2()
 population = initialize_population(cities, population_num)
 # population_fitness = compute_population_fitness(population, distances)
 # fittest = population_fitness[-1]
+fittest = (len(population), 0)
 cnt = 0
 while True:
-    print("iteration number", iteration_num, ":")
     population_fitness = compute_population_fitness(population, distances)
+    if cnt >= stagnancy_num:
+        break
+    elif fittest[1] == population_fitness[-1][1]:
+        cnt += 1
+    else:
+        cnt = 0
     fittest = population_fitness[-1]
+    print("iteration number", iteration_num, ":")
+    print("best fitness generation:", fittest[1])
     mating_pool = rank_based_selection(dict(population_fitness))
     children = crossover(population, mating_pool, p_c, "order")
     children = mutation(children, p_m, "swap")
@@ -28,15 +36,7 @@ while True:
     population = steady_state_replacement(population, population_fitness, \
                                             children,children_fitness, p_rep)
     # print(population)
-    if cnt >= stagnancy_num:
-        break
-    elif fittest == population_fitness[-1]:
-        cnt += 1
-    else:
-        cnt = 0
 
-    fittest = population_fitness[-1]
-    print("best fitness of new generation:", fittest[1])
     iteration_num += 1
 
 print("******************************************")
