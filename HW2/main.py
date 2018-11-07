92e608686fa3643f2045a8f4d397aeebac975aa5
atenagm <ashenatena@gmail.com>
2018-11-07 15:47:32 +0330

    done

--- HW2/main.py
+++ HW2/main.py
@@ -2,11 +2,11 @@ from functions import func1, func2, bounds
 from pso import PSO
 
 # initializtion
-w = 0.5
-c1 = 1
-c2 = 2
+w = 0.2
+c1 = 1.5
+c2 = 2.5
 N = 50
 max_iter = 1000
-stagnancy = 100
+stagnancy = 50
 
-PSO(func1, bounds[0], w, c1, c2, N, max_iter, stagnancy)
+PSO(func2, bounds[1], w, c1, c2, N, max_iter, stagnancy)
