d3cc90745b8cce3709fbce19e06fdd14ed3cf422
atenagm <ashenatena@gmail.com>
2019-01-24 12:04:49 +0330

    implement build_model

--- Project/utils.py
+++ Project/utils.py
@@ -26,7 +26,7 @@ def forward_propagation(X, W, b, func):
     return activation(func, z)
 
 
-def calculate_loss(model, X):
+def calculate_loss(model, X, y):
     W1, b1, W2, b2 = model["W1"], model["b1"], model["W2"], model["b2"]
 
     # forward propagation to hidden layer
@@ -49,3 +49,45 @@ def predict(model, X):
         a2 = forward_propagation(a1, W2, b2, activation_function2)
 
         return np.argmax(a2, axis=1)
+
+
+def build_model(X, y, nn_hdim, num_passes=20000, print_loss=False, batch_gd=False):
+    np.random.seed(0)
+    W1 = np.random.randn(n_input_dim, nn_hdim) / np.sqrt(n_input_dim)
+    b1 = np.zeros((1, nn_hdim))
+    W2 = np.random.randn(nn_hdim, n_output_dim) / np.sqrt(nn_hdim)
+    b2 = np.zeros((1, n_output_dim))
+
+    model = {}
+
+    for i in xrange(0, num_passes):
+        # forward propagation
+        a1 = activation(X, W1, b1, activation_function1)
+        a2 = activation(a1, W2, b2, activation_function2)
+
+        # backpropagation
+        # TODO: put it in a separate function
+        delta3 = a2
+        delta3[range(n_examples), y] -= 1
+        dW2 = (a1.T).dot(delta3)
+        db2 = np.sum(delta3, axis=0, keepdims=True)
+        delta2 = delta3.dot(W2.T) * (1 - np.power(a1, 2))
+        dW1 = np.dot(X.T, delta2)
+        db1 = np.sum(delta2, axis=0)
+
+        # add regularization terms
+        dW2 += reg_lambda * W2
+        dW1 += reg_lambda * W1
+
+        # update parameters
+        W1 += -lr * dW1
+        b1 += -lr * db1
+        W2 += -lr * dW2
+        b2 += -lr * db2
+
+        model = {"W1": W1, "b1": b1, "W2": W2, "b2":b2}
+
+        if print_loss and i % 1000 == 0:
+            print("Loss after iteration {}: {}".format(i, calculate_loss(model, X, y)))
+
+    return model
--- Project/variables.py
+++ Project/variables.py
@@ -3,6 +3,8 @@ n_examples = 200
 n_input_dim = 2
 n_output_dim = 2
 n_hidden_dim = 5
+n_passes = 20000
+print_loss_value = False
 
 lr = 0.01
 reg_lambda = 0.01
